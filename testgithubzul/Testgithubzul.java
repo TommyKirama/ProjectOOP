cc85e662609443aa75fd0106e0403537cff43a74
coderhensemtanpasengaja <156446088+coderhensemtanpasengaja@users.noreply.github.com>
2024-05-25 18:50:39 +0800

    Add files via upload

---
+++ testgithubzul/Testgithubzul.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testgithubzul;
+
+/**
+ *
+ * @author Zulharizi
+ */
+public class Testgithubzul {
+    
+    
+    public static void main(String[] args) {
+        
+        lelaki ali = new lelaki ();
+        System.out.println(ali.perangai);
+        
+   
+                
+                
+    
+    
+    }
+}
